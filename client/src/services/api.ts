83ac91832cc52c9c506c27a43d7a4eede312661a
Eidj <lefevre.antoine@live.fr>
2025-10-27 17:36:24 +0100

    Security: removed keys

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL
-    || (import.meta.env.PROD
-            ? "http://37.59.100.208/api"
-            : "http://localhost:3000"
-    );
-
-const api = axios.create({ baseURL: '/' });
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || '/',
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
 
 export default api;
