4f950b7755196251dd1d62a94368a70b970a8544
Eidj <lefevre.antoine@live.fr>
2025-10-29 17:59:41 +0100

    Fzdlo

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
-    base: './',
+    base: '/app/',
     build: {
         outDir: 'dist',
         emptyOutDir: true,
